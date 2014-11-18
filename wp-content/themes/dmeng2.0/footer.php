7716774c70650e707833637f62649268e0bb93a5
wuxuejian <wuqi570@163.com>
2014-11-18 18:15:53 +0800

    修改分享按钮

--- wp-content/themes/dmeng2.0/footer.php
+++ wp-content/themes/dmeng2.0/footer.php
@@ -11,6 +11,6 @@
 <script src="<?php echo get_bloginfo('template_url').'/js/bootstrap.min.js';?>"></script>
 <script src="<?php echo get_bloginfo('template_url').'/js/dmeng.js';?>?v=2.0.5"></script>
 <?php wp_footer(); ?>
-<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"5","bdPos":"left","bdTop":"107.5"},"viewText":"分享到：","viewSize":"16"},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
+<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"5","bdPos":"left","bdTop":"107.5"},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
 </body>
 </html>
